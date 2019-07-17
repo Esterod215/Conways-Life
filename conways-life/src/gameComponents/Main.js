3c378859fc7cf8eb0f4738b8cfa65f7a975611ae
Estevan <estevan.rodriguez10@yahoo.com>
2019-07-17 01:21:23 -0700

    set up create react appand output Game Of Life on the screen

---
+++ conways-life/src/gameComponents/Main.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom'
+
+class Main extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Game Of Life</h1>
+            </div>
+        )
+    }
+}
+export default Main;
