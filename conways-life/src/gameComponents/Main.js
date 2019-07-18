cbe9690b9fe302cfb9e1b6d195707a4411c39d6d
Estevan <estevan.rodriguez10@yahoo.com>
2019-07-17 18:24:23 -0700

    got the game running

--- conways-life/src/gameComponents/Main.js
+++ conways-life/src/gameComponents/Main.js
@@ -12,11 +12,13 @@ class Main extends Component {
         this.columns = 50;
         this.state = {
             generations: 0,
-            grid: Array(this.rows).fill().map(() => Array(this.columns).fill(false))
+            grid: Array(this.rows).fill().map(() => Array(this.columns).fill(false)),
+            intervalId: 0
         }
     }
     componentDidMount(){
-        this.seed()
+        this.seed();
+        
     }
     selectBox = (row,col) => {
         let gridCopy = arrayClone(this.state.grid);
@@ -39,6 +41,51 @@ class Main extends Component {
             grid: gridCopy
         });
     }
+     
+    gameStart = () => {
+        clearInterval(this.state.intervalId)
+        this.setState({
+            intervalId: setInterval(this.play, this.speed)
+        })
+        
+    }
+    pause = () => {
+        clearInterval(this.state.intervalId);
+    }
+    clear = () => {
+        clearInterval(this.state.intervalId)
+        let gridCopy = arrayClone(this.state.grid);
+        for(let i = 0; i < this.rows; i++) {
+            for(let j = 0; j < this.columns; j++) {
+                gridCopy[i][j] = false;
+            }
+        }
+    }
+
+    play = () => {
+        let g = this.state.grid;
+        let g2 = arrayClone(this.state.grid);
+        for(let i = 0; i < this.rows; i++) {
+            for(let j = 0; j < this.columns; j++) {
+                let count = 0;
+                if (i > 0) if (g[i - 1][j]){ count++;}
+                if (i > 0 && j > 0) if (g[i - 1][j - 1]) {count++};
+                if (i > 0 && j < this.columns - 1) if (g[i - 1][j + 1]) {count++;}
+                if (j < this.columns - 1) if (g[i][j + 1]) {count++;}
+                if (j > 0) if (g[i][j - 1]) {count++;}
+                if (i < this.rows - 1) if (g[i + 1][j]) {count++;}
+                if (i < this.rows - 1 && j > 0) if (g[i + 1][j - 1]) {count++;}
+                if (i < this.rows - 1 && this.columns - 1) if (g[i + 1][j + 1]) {count++;}
+                if (g[i][j] && (count < 2 || count > 3)) {g2[i][j] = false;}
+                if (!g[i][j] && count === 3) {g2[i][j] = true;}
+            }
+        }
+        this.setState({
+            grid: g2,
+            generations: this.state.generations + 1
+        })
+    }
+
     
 
     render() {
@@ -52,6 +99,12 @@ class Main extends Component {
                     selectBox ={this.selectBox}
                 />
                 <h2>Generations: {this.state.generations}</h2>
+                <div className="buttons">
+                <button onClick = {this.gameStart}>Start</button>
+                <button onClick = {this.pause}>Pause</button>
+                <button onClick = {this.clear}>Clear</button>
+                <button onClick={this.seed}>Seed</button>
+                </div>
             </div>
         )
     }
